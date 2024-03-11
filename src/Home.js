343f4728cb09e56b5999d009fd148ba12152e7b7
Lavanyag188 <lavi22275@gmail.com>
2024-03-11 16:36:45 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://images.pexels.com/photos/2247179/pexels-photo-2247179.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Welcome picture" />
+    </div>
+  )
+}
+
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API = "https://fitness-backend-pied.vercel.app"
